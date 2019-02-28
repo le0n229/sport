4532d964064f6ced7e9e615fc6362aa2dfa12dca
Rauf Erk <erk.rauf@gmail.com>
2019-02-28 14:16:21 +0300

    back for new order

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Users = require('../models/clients');
+const Orders = require('../models/order');
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
@@ -28,15 +29,20 @@ router.post('/profile', async function (req, res, next) {
 });
 
 router.post('/order', async function (req, res, next) {
-  Users.updateOne({ id: req.session.user.id }, {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    address: req.body.address,
-    phone: req.body.phone,
-    age: req.body.age,
-    height: req.body.height,
-    weight: req.body.weight,
-    gender: req.body.gender,
+  Orders.updateOne({ id: req.session.user.id }, {
+    userName: req.session.user.userName,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    status: 'new',
+    orderNumber: 0,
+    deliveryDate: req.body.deliveryDate,
+    deliveryTime: req.body.deliveryTime,
+    address: req.body.deliveryAddress,
+    firstName: String,
+    lastName: String,
+    phone: String,
+    orderAmount: 0,
+    orderTitle: req.body.serviceType
   }, function (err, result) {
     if (err) return console.log(err);
     console.log(result);
