1086e86f3cd97e783d5ced044344dc774a20c101
le0n229 <leon229@yandex.ru>
2019-02-28 15:01:38 +0300

    menu

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ hbs.registerHelper('ifCond', function(v1, v2, options) {
   return options.inverse(this);
 });
 
+hbs.registerHelper('ifundef', function(v1, options) {
+  if(v1 == undefined) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 // const app = express();
 
 const db = mongoose.connect(
